9510c3371f8cbfe9c16291d386a388442309173a
Van Hien <hiench2001@gmail.com>
2023-06-15 14:19:33 -0600

    created member models

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(req):
-    return HttpResponse("Hello, world!")
+def say_hello(request):
+    template = loader.get_template('hello.html')
+    return HttpResponse(template.render())
