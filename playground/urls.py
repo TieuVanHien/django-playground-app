07c3033345976eabbf0dd62e11d1ce1e38e68143
Van Hien <hiench2001@gmail.com>
2023-06-16 15:11:55 -0600

    added main page and path

--- playground/urls.py
+++ playground/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 #url config
 urlpatterns = [
-    path('', views.members, name='members'),
-    path('details/<int:id>', views.details, name='details'),
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
 ]
